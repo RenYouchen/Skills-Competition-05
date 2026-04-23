c73f056fca5d7bcab9757bf782e76a84e3d02ef2
BrianRen <renyouchen3@gmail.com>
2026-04-23 15:33:22 +0800

    Q4 Finish

--- BrianRen_114_Q4/Form1.Designer.cs
+++ BrianRen_114_Q4/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(165, 32);
+            label1.TabIndex = 0;
+            label1.Text = "輸入分數a/b=";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(183, 12);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(411, 102);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(183, 313);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(411, 102);
+            richTextBox2.TabIndex = 3;
+            richTextBox2.Text = "";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 313);
+            label2.Name = "label2";
+            label2.Size = new Size(155, 32);
+            label2.TabIndex = 2;
+            label2.Text = "輸出連分數=";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(183, 176);
+            button1.Name = "button1";
+            button1.Size = new Size(146, 105);
+            button1.TabIndex = 4;
+            button1.Text = "計算";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(448, 176);
+            button2.Name = "button2";
+            button2.Size = new Size(146, 105);
+            button2.TabIndex = 5;
+            button2.Text = "清除";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(richTextBox2);
+            Controls.Add(label2);
+            Controls.Add(richTextBox1);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private Label label2;
+        private Button button1;
+        private Button button2;
     }
 }
--- BrianRen_114_Q4/Form1.cs
+++ BrianRen_114_Q4/Form1.cs
@@ -6,5 +6,24 @@ namespace BrianRen_114_Q4
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            (a, b) = richTextBox1.Text.Split("/").Select(x => int.Parse(x)).ToList() switch { var x => (x[0], x[1]) };
+
+            string output = "";
+            output += $"{a / b};";
+            while (true)
+            {
+                var tmp = b;
+                b = a % b;
+                a = tmp;
+                if (a == 0 || b == 0) break;
+                output += $"{a / b},";
+            }
+            output = output.Remove(output.Length - 1);
+            richTextBox2.Text = output;
+        }
     }
 }
