2f146795bd350dca1bc69e783d9b6dc37f13dbdb
renyouchen <renyouchen3@gmail.com>
2026-05-05 16:18:58 +0800

    read text

--- BrianRen_114_Q2/Program.cs
+++ BrianRen_114_Q2/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Read file
+Console.Write("Enter 1st filename:");
+var file1 = Console.ReadLine();
+Console.Write("Enter 2st filename:");
+var file2 = Console.ReadLine();
+
+List<string> rawStr1 = File.ReadAllLines($"../../../{file1}").ToList();
+// Console.WriteLine(string.Join('\n', rawStr1));
+
