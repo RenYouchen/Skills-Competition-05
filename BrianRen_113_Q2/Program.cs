b135b4aafd9287e335dd78dd7a46ce64752b87ff
BrianRen <renyouchen3@gmail.com>
2026-05-12 15:37:18 +0800

    init 113_Q2

---
+++ BrianRen_113_Q2/Program.cs
@@ -0,0 +1,15 @@
+﻿int weightTotal, n;
+List<int> w, v = new List<int>();
+
+Console.Write("輸入物品的總數量：");
+n = int.Parse(Console.ReadLine());
+Console.Write("輸入背包的最大承載量（10的倍數）：");
+weightTotal = int.Parse(Console.ReadLine());
+Console.WriteLine("輸入各物品的重量與價值：");
+for(int i = 1; i <= n; i++)
+{
+    Console.Write($"物品{i} - 重量：");
+    w.Add(int.Parse(Console.ReadLine()));
+    Console.Write($"物品{i} - 價值：");
+    v.Add(int.Parse(Console.ReadLine()));
+}
