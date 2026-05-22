910eecf78265f19840afb2312b24f6050021561c
renyouchen <renyouchen3@gmail.com>
2026-05-22 21:08:14 +0800

    finish part 1

--- BrianRen_113_Q3/Program.cs
+++ BrianRen_113_Q3/Program.cs
@@ -1,6 +1,46 @@
-﻿List<List<int>> box = Enumerable.Repeat(Enumerable.Repeat(0, 3).ToList(), 3).ToList();
+﻿List<List<int>> box = Enumerable.Range(0, 3).Select(_ => Enumerable.Repeat(0, 3).ToList()).ToList();
+int x = 0;
+int y = 1;
+
+for (int i = 1; i <= 9; i++)
+{
+    Console.WriteLine($"x:{x} y:{y}");
+    box[x][y] = i;
+    
+    foreach (var a in box)
+    {
+        Console.WriteLine(string.Join(' ', a));
+    }
+    
+    ChangePos(ref x, ref y);
+    
+    Console.WriteLine("--------");
+}
 
 
+
+void ChangePos(ref int x, ref int y)
+{
+    int oldX = x;
+    int oldY = y;
+    
+    x -= 1;
+    y += 1;
+    
+    if ((x>=0 && x <=2 && y>=0 && y<=2 && box[x][y] != 0) || (x < 0 && y > 2))
+    {
+        x = oldX+1;
+        y = oldY;
+    }
+    if (x < 0)
+    {
+        x = 2;
+    } 
+    if (y > 2)
+    {
+        y = 0;
+    }
+}
 
 foreach (var i in box)
 {
