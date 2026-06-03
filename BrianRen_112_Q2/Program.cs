619d5ad054843b61bef4d7793d1dab5773aff194
renyouchen <renyouchen3@gmail.com>
2026-06-03 15:24:52 +0800

    test

--- BrianRen_112_Q2/Program.cs
+++ BrianRen_112_Q2/Program.cs
@@ -1,4 +1,33 @@
 ﻿Console.Write("輸入原來的英文單字：");
 string s1 = Console.ReadLine();
+s1.Insert(0, " ");
 Console.Write("輸入改變後的英文單字：");
-string s2 = Console.ReadLine();
+string s2 = Console.ReadLine(); 
+s2.Insert(0, " ");
+
+List<List<int>> dp = Enumerable.Repeat(Enumerable.Repeat(0,s1.Length).ToList(), s2.Length).ToList();
+
+for (int i = 0; i < s1.Length; i++)
+{
+    for (int j = 0; j < s2.Length; j++)
+    {
+        Console.WriteLine($"{i} {j}");
+        if (i == 0 && j == 0) continue;
+        if (i == 0)
+        {
+            dp[i][j] = dp[i][j - 1] + 1;
+            continue;
+        }
+        if (j == 0)
+        {
+            dp[i][j] = dp[i - 1][j] + 1;
+            continue;
+        }
+        dp[i][j] = Math.Min(
+            Math.Min(dp[i-1][j] +1, dp[i][j-1]+1),
+            dp[i-1][j-1] + s1[i] == s2[j] ? 0 : 2
+        );
+    }
+}
+
+Console.WriteLine($"編輯距離：{dp[s1.Length-1][s2.Length-1]}");
--- ConsoleApp_Test/Program.cs
+++ ConsoleApp_Test/Program.cs
@@ -4,3 +4,8 @@ Console.WriteLine(string.Join(" ",nList.Select(x => x += 1).ToList()));
 
 int a, b;
 (a, b) = nList switch { var x => (x[0], x[1]) };
+string s="123456";
+var test = s.OrderByDescending(x => x);
+Console.WriteLine($"a:{a} b:{b}");
+Console.WriteLine(string.Join(' ', test));
+Console.WriteLine($"{4:D5}");
