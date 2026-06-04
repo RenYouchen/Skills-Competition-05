71de578bb739963e15e1c8096e8c1e42996f94c5
renyouchen <renyouchen3@gmail.com>
2026-06-04 17:43:15 +0800

    sort file and init 114_Q6_R2 file

--- ConsoleApp_Test/Program.cs
+++ ConsoleApp_Test/Program.cs
@@ -8,4 +8,4 @@ string s="123456";
 var test = s.OrderByDescending(x => x);
 Console.WriteLine($"a:{a} b:{b}");
 Console.WriteLine(string.Join(' ', test));
-Console.WriteLine($"{4:D5}");
+Console.WriteLine($"{123.4567:P}");
