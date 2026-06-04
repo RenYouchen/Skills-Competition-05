7b71cfe85391a27868e5cdf327e73f5adf071a81
renyouchen <renyouchen3@gmail.com>
2026-06-04 18:57:13 +0800

    a

--- BrianRen_114_Q6_Round2/Program.cs
+++ BrianRen_114_Q6_Round2/Program.cs
@@ -1,1 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
+
+record Data(int ID, string ObjType, string MaType, float Para1, float Para2)
+{
+    private List<string> objs = ["Ball", "Cube", "Pyramid", "Cylinder"];
+}
