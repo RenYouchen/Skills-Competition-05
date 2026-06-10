2fdaa741469339563fbde618ef5acd741aac2114
BrianRen <renyouchen3@gmail.com>
2026-06-10 21:31:56 +0800

    Finish Q3 Round2 (Double DFS res)

---
+++ BrianRen_114_Q3_Round2/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("請輸入節點數N：");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("請輸入N-1行血緣關係(每行兩個整數，以空格分割)：");
+List<List<int>> Graph = Enumerable.Repeat(0, N + 1).Select(_ => new List<int>()).ToList();
+
+for (int i = 0; i < N-1; i++)
+{
+    var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+    Graph[input[0]].Add(input[1]);
+    Graph[input[1]].Add(input[0]);
+}
+
+List<bool> Visited = Enumerable.Repeat(false, N + 1).ToList();
+
+(int, int) DFS(int CurrentNode, int depth)
+{
+    Visited[CurrentNode] = true;
+    int farestNode = CurrentNode;
+    int maxDepth = depth;
+    foreach (var v in Graph[CurrentNode])
+    {
+        if (!Visited[v]) {
+            var (x,y) = DFS(v, depth + 1);
+            if(y>maxDepth)
+            {
+                (farestNode, maxDepth) = (x, y);
+            }
+        };
+    }
+
+    return (farestNode, maxDepth);
+}
+
+int a = DFS(1, 0).Item1;
+for (int i = 0; i < Visited.Count; i++)
+{
+    Visited[i] = false;
+}
+Console.WriteLine(DFS(a, 0));
