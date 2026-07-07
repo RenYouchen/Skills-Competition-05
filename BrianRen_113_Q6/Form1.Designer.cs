745bbdd72856bd20acbb7d5a325e53b6844c129c
BrianRen <renyouchen3@gmail.com>
2026-07-07 15:06:31 +0800

    init 113 Q6 and UI

---
+++ BrianRen_113_Q6/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace BrianRen_113_Q6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(56, 51);
+            button1.Name = "button1";
+            button1.Size = new Size(243, 102);
+            button1.TabIndex = 0;
+            button1.Text = "讀取影像";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.Location = new Point(56, 198);
+            button2.Name = "button2";
+            button2.Size = new Size(243, 102);
+            button2.TabIndex = 1;
+            button2.Text = "儲存印象";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaption;
+            button3.Location = new Point(56, 395);
+            button3.Name = "button3";
+            button3.Size = new Size(243, 102);
+            button3.TabIndex = 3;
+            button3.Text = "灰階轉換";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveCaption;
+            button4.Location = new Point(56, 549);
+            button4.Name = "button4";
+            button4.Size = new Size(243, 102);
+            button4.TabIndex = 2;
+            button4.Text = "邊緣檢測";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(578, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(114, 32);
+            label1.TabIndex = 4;
+            label1.Text = "輸入影像";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(385, 139);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(500, 500);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(948, 139);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(500, 500);
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(1141, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 32);
+            label2.TabIndex = 6;
+            label2.Text = "輸入影像";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1505, 706);
+            Controls.Add(pictureBox2);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label2;
+    }
+}
---
+++ BrianRen_113_Q6/Form1.cs
@@ -0,0 +1,10 @@
+namespace BrianRen_113_Q6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
