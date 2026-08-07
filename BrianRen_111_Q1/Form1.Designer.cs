ef55bbfd2e6a8aa3fd3fc7888f315ae89a001abc
BrianRen <renyouchen3@gmail.com>
2026-08-07 23:46:45 +0800

    finish 111 Q1

--- BrianRen_111_Q1/Form1.Designer.cs
+++ BrianRen_111_Q1/Form1.Designer.cs
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label1.Location = new Point(47, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(148, 45);
+            label1.TabIndex = 0;
+            label1.Text = "輸入 n =";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(201, 47);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 39);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label2.Location = new Point(48, 111);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 45);
+            label2.TabIndex = 2;
+            label2.Text = "輸出";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label3.Location = new Point(152, 184);
+            label3.Name = "label3";
+            label3.Size = new Size(1377, 45);
+            label3.TabIndex = 3;
+            label3.Text = "A      = [                                                                                                                                        ]";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label4.Location = new Point(173, 152);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 45);
+            label4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label5.Location = new Point(295, 152);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 45);
+            label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label6.Location = new Point(909, 152);
+            label6.Name = "label6";
+            label6.Size = new Size(0, 45);
+            label6.TabIndex = 6;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label7.Location = new Point(295, 229);
+            label7.Name = "label7";
+            label7.Size = new Size(0, 45);
+            label7.TabIndex = 7;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label8.Location = new Point(909, 229);
+            label8.Name = "label8";
+            label8.Size = new Size(0, 45);
+            label8.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1660, 450);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
     }
 }
--- BrianRen_111_Q1/Form1.cs
+++ BrianRen_111_Q1/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace BrianRen_111_Q1
 {
     public partial class Form1 : Form
@@ -5,6 +7,35 @@ namespace BrianRen_111_Q1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label4.Text = textBox1.Text;
+            if (textBox1.Text == "") return;
+            long[] result = new long[] { 1, 1, 1, 0 };
+            for (int i = 0; i < int.Parse(textBox1.Text) -1; i++)
+            {
+                result = calc(result, new long[] { 1, 1, 1, 0 });
+            }
+            label5.Text = result[0].ToString();
+            label6.Text = result[1].ToString();
+            label7.Text = result[2].ToString();
+            label8.Text = result[3].ToString();
+
+        }
+        long[] calc(long[] a, long[] b)
+        {
+            return new[]
+            {
+                a[0]*b[0] + a[1]*b[2], a[0]*b[1] + a[1]*b[3],
+                a[2]*b[0] + a[3]*b[2],  a[2]*b[1] + a[3]*b[3]
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
