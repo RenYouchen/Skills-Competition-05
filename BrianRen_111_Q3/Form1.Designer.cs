663e46a77463a6e0885b05a3769cc12ec5eb31e4
BrianRen <renyouchen3@gmail.com>
2026-08-28 05:51:31 +0800

    111 Q3 finish

--- BrianRen_111_Q3/Form1.Designer.cs
+++ BrianRen_111_Q3/Form1.Designer.cs
@@ -28,12 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 13);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(496, 45);
+            label1.TabIndex = 1;
+            label1.Text = "具有翻轉和旋轉之圖形編輯系統";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(13, 62);
+            pictureBox1.Margin = new Padding(4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(240, 240);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox2.Location = new Point(261, 62);
+            pictureBox2.Margin = new Padding(4);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(240, 240);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(521, 13);
+            button1.Name = "button1";
+            button1.Size = new Size(272, 59);
+            button1.TabIndex = 3;
+            button1.Text = "Random";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(521, 78);
+            button2.Name = "button2";
+            button2.Size = new Size(272, 59);
+            button2.TabIndex = 4;
+            button2.Text = "Hor-Flip";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(521, 143);
+            button3.Name = "button3";
+            button3.Size = new Size(272, 59);
+            button3.TabIndex = 5;
+            button3.Text = "Ver-FLip";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(520, 208);
+            button4.Name = "button4";
+            button4.Size = new Size(272, 59);
+            button4.TabIndex = 6;
+            button4.Text = "CW-Rotate-90";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(521, 273);
+            button5.Name = "button5";
+            button5.Size = new Size(272, 59);
+            button5.TabIndex = 7;
+            button5.Text = "CCW-Rotate-90";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(521, 338);
+            button6.Name = "button6";
+            button6.Size = new Size(272, 59);
+            button6.TabIndex = 8;
+            button6.Text = "Exit";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(19F, 45F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(805, 640);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(4);
+            Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
     }
 }
--- BrianRen_111_Q3/Form1.cs
+++ BrianRen_111_Q3/Form1.cs
@@ -2,9 +2,129 @@ namespace BrianRen_111_Q3
 {
     public partial class Form1 : Form
     {
+        Color[,] colors = new Color[4, 4];
+        Color[,] oldColors;
         public Form1()
         {
             InitializeComponent();
+            create();
+            pictureBox2.Image = draw(colors);
+            swap_old_color();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            create();
+            swap_old_color();
+        }
+
+        private void swap_old_color()
+        {
+            if (oldColors != null) pictureBox1.Image = draw(oldColors);
+            oldColors = (Color[,])colors.Clone();
+            pictureBox2.Image = draw(colors);
+        }
+        private void create()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    colors[i, j] = Color.FromArgb(
+                        Random.Shared.Next(255),
+                        Random.Shared.Next(255),
+                        Random.Shared.Next(255)
+                        );
+                }
+            }
+        }
+
+        private Bitmap draw(Color[,] colors)
+        {
+            Bitmap img = new Bitmap(240, 240);
+            Graphics g = Graphics.FromImage(img);
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    g.FillRectangle(new SolidBrush(colors[i, j]), i * 60, j * 60, 60, 60);
+                }
+            }
+            //pictureBox2.Image = img;
+            return img;
+        }
+
+        //HoriFlip
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    (colors[i, j], colors[3 - i, j]) = (colors[3 - i, j], colors[i, j]);
+                }
+            }
+            swap_old_color();
+        }
+
+        //VerFlip
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    (colors[j, i], colors[j, 3 - i]) = (colors[j, 3 - i], colors[i, j]);
+                }
+            }
+            swap_old_color();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Color[,] newArray = new Color[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    newArray[i, j] = colors[j, i];
+                }
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    (newArray[i, j], newArray[3 - i, j]) = (newArray[3 - i, j], newArray[i, j]);
+                }
+            }
+            colors = newArray;
+            swap_old_color();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Color[,] newArray = new Color[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    newArray[i, j] = colors[j, i];
+                }
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    (newArray[j, i], newArray[j, 3 - i]) = (newArray[j, 3 - i], newArray[i, j]);
+                }
+            }
+            colors = newArray;
+            swap_old_color();
         }
     }
 }
