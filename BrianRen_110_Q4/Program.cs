e9565521f53ef64a3c68b10b4f2de9a3f040425d
renyouchen <renyouchen3@gmail.com>
2026-09-01 21:57:28 +0800

    init 110 Q4 & add test file

---
+++ BrianRen_110_Q4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+Console.Write("請輸入一個二維陣列：");
+var rawInput = Console.ReadLine();
+var map = JsonSerializer.Deserialize<int[][]>(rawInput);
+Console.WriteLine(string.Join('\n', map.Select(x=>$"[{string.Join(", ", x)}]")));
+
+List<List<int>> Parse(string input)
+{
+    List<List<int>> map = new List<List<int>>();
+    
+    
+    return map;
+}
