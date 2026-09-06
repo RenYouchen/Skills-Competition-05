c951b5d813ef2bff308d1185d72a5a9685a648fd
renyouchen <renyouchen3@gmail.com>
2026-09-06 19:51:35 +0800

    110 Q6 Finish

---
+++ BrianRen_110_Q6/Program.cs
@@ -0,0 +1,16 @@
+﻿var s = "";
+Stack<(int, int)> stack = new Stack<(int, int)>();
+//[0] == in time, [1] == out time
+while ((s = Console.ReadLine()) != "")
+{
+    var input = s.Split(' ').Select(int.Parse).ToList();
+    if (stack.Count == 0 || input[0] >= stack.Peek().Item2 || stack.Peek().Item2 >= input[1])
+    {
+        stack.Push((input[0],input[1]));
+    }
+}
+Console.WriteLine(stack.Count);
+
+// input[0] <=stack.Peek().Item1
+
+
