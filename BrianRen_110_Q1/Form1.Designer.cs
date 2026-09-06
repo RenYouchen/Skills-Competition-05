7c54dc7e701fee0e5add885d2e5e8718c0028b71
BrianRen <renyouchen3@gmail.com>
2026-09-07 01:27:54 +0800

    finish 110 Q1

--- BrianRen_110_Q1/Form1.Designer.cs
+++ BrianRen_110_Q1/Form1.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label1
@@ -36,15 +42,75 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 37);
+            this.label1.Size = new System.Drawing.Size(314, 37);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "水平主桿樹連線系統";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(637, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(267, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "總長度（#網格）";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(625, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 44);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(644, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 101);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Random Nodes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(644, 248);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 101);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "H-Trunk Tree";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(644, 355);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(239, 101);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 7;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1333, 666);
+            this.ClientSize = new System.Drawing.Size(916, 668);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
@@ -58,6 +124,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- BrianRen_110_Q1/Form1.cs
+++ BrianRen_110_Q1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.PerformanceData;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,90 @@ namespace BrianRen_110_Q1
         public Form1()
         {
             InitializeComponent();
+            panel1.Refresh();
+        }
+
+        List<(int,int)> points = new List<(int,int)> ();
+        List<(Point, Point)> lines = new List<(Point, Point)> ();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var rnd = new Random();
+            points.Clear();
+            lines.Clear();
+            List<int> x = Enumerable
+                .Range(1, 8)
+                .OrderBy(_=>rnd.Next())
+                .ToList();
+            List<int> y = Enumerable
+                .Range(1, 8)
+                .OrderBy(_=>rnd.Next())
+                .ToList();
+            for(int i = 0; i < x.Count; i++)
+            {
+                points.Add((x[i], y[i]));
+            }
+            points.RemoveAt(rnd.Next(0, 7));
+            points = points.OrderBy(i => i.Item1).ToList();
+            panel1.Refresh();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    e.Graphics.DrawRectangle(
+                        new Pen(color: Color.Black),
+                        new Rectangle(i * 60,j * 60, 60,60)
+                    );
+                }
+            }
+            if (lines.Count > 0)
+            {
+                for (int i = 0; i < lines.Count; i++)
+                {
+                    e.Graphics.DrawLine(
+                        new Pen(Color.Red, 4),
+                        lines[i].Item1, lines[i].Item2
+                        );
+                }
+            }
+            if (points.Count > 0)
+            { 
+                for (int i = 0; i < points.Count; i++) {
+                    e.Graphics.FillEllipse(
+                        new SolidBrush(Color.Black),
+                        points[i].Item1 * 60 - 5,
+                        points[i].Item2 * 60 - 5,
+                        10,10
+                        );
+                }
+            }
+
+                
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lines.Clear();
+            if (points.Count == 0) return;
+            int count = 0;
+            var y = (int)points.Average(x=>x.Item2);
+            var x1 = points.First().Item1;
+            var x2 = points.Last().Item1;
+            count += x2-x1;
+            lines.Add((new Point(x1*60, y*60), new Point(x2*60, y*60)));
+            foreach (var p in points) {
+                count += Math.Abs(p.Item2 - y);
+                lines.Add((new Point(p.Item1 * 60, y * 60), new Point(p.Item1 * 60, p.Item2 * 60)));
+            }
+
+            textBox1.Text = count.ToString();
+            panel1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
