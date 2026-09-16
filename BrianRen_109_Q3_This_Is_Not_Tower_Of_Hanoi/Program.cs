038a27b3e532b910aea8a4bae1a46542d26bc713
renyouchen <renyouchen3@gmail.com>
2026-09-16 23:10:18 +0800

    finish 109 Q3 這不是河內塔喔

---
+++ BrianRen_109_Q3_This_Is_Not_Tower_Of_Hanoi/Program.cs
@@ -0,0 +1,28 @@
+﻿string raw;
+while ((raw = Console.ReadLine()) != "")
+{
+    int n = int.Parse(raw);
+    string rawInput;
+    while ((rawInput = Console.ReadLine()) != "0")
+    {
+        List<int> inputs = rawInput.Split(' ').Select(int.Parse).Reverse().ToList();
+        Stack<int> towerB = new Stack<int>();
+        List<int> nums = Enumerable.Range(1, n).ToList();
+        for (int i = 0; i < n; i++)
+        {
+            towerB.Push(nums[i]);
+            while (towerB.Count != 0)
+            {
+                if (towerB.Peek() != inputs[0])
+                {
+                    break;
+                }
+                else
+                {
+                    inputs.Remove(towerB.Pop());
+                }
+            }
+        }
+        Console.WriteLine(towerB.Count==0?"YES!":"NO!");
+    } 
+}
